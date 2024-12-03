8f6e524f51c37faddd0ba6fc63ce9841eca5e438
six-shot <okhuomonajayi54@gmail.com>
2024-12-03 07:42:23 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
-import './App.css';
-import Home from './modules/home';
+import "./App.css";
+import Footer from "./modules/components/footer";
+import Home from "./modules/home";
 
 function App() {
   return (
     <div>
       <Home />
- 
+      <Footer />
     </div>
   );
 }
--- src/modules/components/footer/index.jsx
+++ src/modules/components/footer/index.jsx
@@ -1,1 +1,23 @@
-rfc
+import React from 'react'
+import logo from "../../../assets/logo.png";
+
+export default function Footer() {
+  return (
+    <div className=" mt-[70px]">
+      <div className="max-w-[1440px] mx-auto px-[150px] py-10">
+        <div className="flex items-center justify-between">
+          <img className="w-[60px]" src={logo} alt="logo" />
+          <nav className="flex gap-7 items-center text-sm text-white">
+            <h5>Home</h5>
+            <h5>Home</h5>
+            <h5>Home</h5>
+            <h5>Home</h5>
+          </nav>
+        </div>
+        <p className="text-center text-white mt-[60pz]">
+          © DEFI AI . All Rights Reserved 2024. Licensing
+        </p>
+      </div>
+    </div>
+  );
+}
